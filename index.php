7ea2a6be57f5a8838d3a36135a5c0985b0a9868f
41419839 <41419839@090011-P33S13.macklabs.br>
2015-03-12 11:50:53 -0300

    index.php e esqueleto do CSS montados.

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="Pokémon Damage Calculator">
+        <meta name="keywords" content="Pokémon,Damage,Calculator">
+        <meta name="author" content="GRUPO ZIKA">
+        <title>Pokémon Damage Calculator</title>
+        
+        <link rel="stylesheet" type="text/css" href="style.css">
+        
+        <link rel="icon" 
+        type="image/png" 
+        href="favicon.ico">
+    </head>
+    
+    <body>
+        <header>HEADERHEADERHEADERHEADERHEADERHEADER</header>
+        
+        <menu id="topmenu">TOPMENUTOPMENUTOPMENU</menu>
+        
+        <menu id="sidemenu">SIDEMENUSIDEMENU</menu>
+        
+        <section id="formcontainer">FORMCONTAINERFORMCONTAINERFORMCONTAINER</section>
+        
+        <!-- <footer>FOOTERFOOTERFOOTERFOOTERFOOTERFOOTER</footer> -->
+    </body>
+    
+</html>
