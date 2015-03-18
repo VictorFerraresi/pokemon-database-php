cd67252a6418cc096a12d5e969ba2214c5ed53d1
41440927 <41440927@090012-P33S13.macklabs.br>
2015-03-18 10:16:15 -0300

    Adicionado o esqueleto básico de formulário.

--- index.php
+++ index.php
@@ -22,7 +22,39 @@
         
         <menu id="sidemenu">SIDEMENUSIDEMENU</menu>
         
-        <section id="formcontainer">FORMCONTAINERFORMCONTAINERFORMCONTAINER</section>
+        <section id="formcontainer">
+            <p>
+                Este <i>site</i> foi criado com o propósito de auxiliar jogadores dos jogos 
+               da franquia de Pokémon que se dispõe a dedicar seu tempo ao ambiente competitivo. 
+               Aqui você pode escolher seu Pocket Monster favorito e montá-lo com seu <i>set</i> de golpes, 
+               habilidade, natureza, itens, e simular o golpe que o tornará efetivo a gosto contra seu adversário.
+            </p>
+            <form id="form">
+                Level do Pokémon atacante:
+                <input type="text" name="lvl" id="input"><br><br>
+                Valor do atributo atk/sp.atk:
+                <input type="text" name="atk" id="input"><br><br>
+                Valor do atributo def/sp.def:
+                <input type="text" name="def" id="input"><br><br>
+                Dano base do golpe:
+                <input type="text" name="dmg" id="input"><br><br>
+                Stab:
+                <input type="checkbox" name="stab" value="yes" id="input"><br><br>
+                Modificadores de tipo:
+                <select id="input">
+                    <option value="1">1</option>
+                    <option value="0.25">0.25</option>
+                    <option value="0.5">0.5</option>
+                    <option value="2">2</option>
+                    <option value="4">4</option>
+                </select><br><br>
+                Golpe crítico:
+                <input type="checkbox" name="critico" value="yes" id="input"><br><br>
+                <input type="submit" value="Calcular" id="input">
+                
+                
+            </form>
+        </section>
         
         <!-- <footer>FOOTERFOOTERFOOTERFOOTERFOOTERFOOTER</footer> -->
     </body>
