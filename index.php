39eb2b6d2be0837aadc211347355039b7acccc73
41419839 <41419839@093192-P31S203.macklabs.br>
2015-04-02 09:04:23 -0300

    Twitter bootstrap adicionado, em desenvolvimento de tema.

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <!DOCTYPE html>
+<?php
+    session_start();
+?>
 <html lang="pt-br">
     
     <head>
@@ -17,63 +20,100 @@
     </head>
     
     <body>
-        <header>HEADERHEADERHEADERHEADERHEADERHEADER</header>
-      
-        <menu id="sidemenu">           
-        </menu>
-        
-        <div class="container">
-
-      <form class="form-signin" id="form" method="post" action="calcuate.php">
-        <h2 class="form-signin-heading">Insira as informações</h2>
-        <label for="inputlvl" class="sr-only">Level do Pokémon atacante</label>
-        <input type="text" id="inputlvl" class="form-control" name="lvl" placeholder="Level do Pokémon Atacante" required autofocus>
-        <label for="inputatk" class="sr-only">Ataque</label>
-        <input type="text" id="inputatk" class="form-control" name="atk" placeholder="Valor do atributo atk/sp.atk" required>
-        <label for="inputdef" class="sr-only">Defesa</label>
-        <input type="text" id="inputdef" class="form-control" name="def" placeholder="Valor do atributo def/sp.def" required>
-        <label for="inputdmg" class="sr-only">Dano Base</label>
-        <input type="text" id="inputdmg" class="form-control" name="dmg" placeholder="Dano base do golpe" required>
-        <div class="checkbox">
-          <label>
-            <input type="checkbox" name = "stab" id ="inputstab" value="yes"> Stab
-          </label>
-          <label>
-            <input type="checkbox" name = "critico" id ="inputcrit" value="yes"> Golpe Crítico
-          </label>
-          <select id="input" name="type">
-                    <option value="1">1</option>
-                    <option value="0.25">0.25</option>
-                    <option value="0.5">0.5</option>
-                    <option value="2">2</option>
-                    <option value="4">4</option>
-           </select>
-            
+        <!-- Fixed navbar -->
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Bootstrap theme</a>
         </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Calcular!</button>
-      </form>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+      
+        <div class="jumbotron">
+            <h1>Pokémon Damage Calculator</h1>
+            <p>Este <i>site</i> foi criado com o propósito de auxiliar jogadores dos jogos 
+           da franquia de Pokémon que se dispõe a dedicar seu tempo ao ambiente competitivo. 
+           Aqui você pode escolher seu Pocket Monster favorito e montá-lo com seu <i>set</i> de golpes, 
+           habilidade, natureza, itens, e simular o golpe que o tornará efetivo a gosto contra seu adversário.</p>
+        </div> 
+        
+            <div class="container">
 
-    </div> <!-- /container -->
-        
-        <section id="formcontainer">
-            <p>
-                Este <i>site</i> foi criado com o propósito de auxiliar jogadores dos jogos 
-               da franquia de Pokémon que se dispõe a dedicar seu tempo ao ambiente competitivo. 
-               Aqui você pode escolher seu Pocket Monster favorito e montá-lo com seu <i>set</i> de golpes, 
-               habilidade, natureza, itens, e simular o golpe que o tornará efetivo a gosto contra seu adversário.
-            </p> 
-            <?php
-                session_start();
-                if(isset($_SESSION['showresult'])){				
-                    if($_SESSION['showresult'] != 0){
-                        echo '<p>'.$_SESSION['showresult'].'</p>';
-                        unset($_SESSION['showresult']);
-                    } 
-		}
-            ?>
-        </section>
-        
+            <form class="form-signin" id="form" method="post" action="calculate.php">
+              <h2 class="form-signin-heading">Insira as informações</h2>
+              <label for="inputlvl" class="sr-only">Level do Pokémon atacante</label>
+              <input type="text" id="inputlvl" class="form-control" name="lvl" placeholder="Level do Pokémon Atacante" required autofocus>
+              <label for="inputatk" class="sr-only">Ataque</label>
+              <input type="text" id="inputatk" class="form-control" name="atk" placeholder="Valor do atributo atk/sp.atk" required>
+              <label for="inputdef" class="sr-only">Defesa</label>
+              <input type="text" id="inputdef" class="form-control" name="def" placeholder="Valor do atributo def/sp.def" required>
+              <label for="inputdmg" class="sr-only">Dano Base</label>
+              <input type="text" id="inputdmg" class="form-control" name="dmg" placeholder="Dano base do golpe" required>
+              <div class="checkbox">
+                <label>
+                    <input type="checkbox" name = "stab" id ="inputstab" value="yes"> Stab
+                </label>
+                <br>                     
+                <label>
+                    <input type="checkbox" name = "critico" id ="inputcrit" value="yes"> Golpe Crítico
+                </label>
+                <br>
+                <label for="inputmod" class="sr-only">Modificador de Dano</label>
+                Modificador de Dano
+                <select id="inputmod" name="type">
+                          <option value="1">1</option>
+                          <option value="0.25">0.25</option>
+                          <option value="0.5">0.5</option>
+                          <option value="2">2</option>
+                          <option value="4">4</option>
+                 </select>
+                 <?php                    
+                    if(isset($_SESSION['showresult'])){				
+                        if($_SESSION['showresult'] != 0){
+                            echo '<div class="alert alert-info" role="alert">
+                                <strong>Resutado do Dano: </strong>' .$_SESSION['showresult'].'
+                                </div>';
+                            unset($_SESSION['showresult']);
+                        } 
+                    }
+                ?>
+
+              </div>
+              <button class="btn btn-lg btn-primary btn-block" type="submit" name="submit">Calcular!</button>
+            </form>
+
+        </div> <!-- /container -->
+
         <!-- <footer>FOOTERFOOTERFOOTERFOOTERFOOTERFOOTER</footer> -->
     </body>
-    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="../../assets/js/docs.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
 </html>
