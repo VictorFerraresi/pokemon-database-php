3e11b96604afcdddc2dab715eecde1924b8a26f3
41440927 <41440927@089828-P33S09.macklabs.br>
2015-05-06 10:23:01 -0300

    Footer dinâmico adicionado.

---
+++ randomFrase.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+            <div class="container">
+                <p class="text-muted">
+                    <?php
+                    $x=rand(1, 20);
+                    
+                    switch ($x) {
+                        case 1:
+                            echo 'Hit me with your best shot!';
+                            break;
+                        case 2:
+                            echo 'Hit me with your best shot!';
+                            break;
+                        case 3:
+                            echo 'Hit me with your best shot!';
+                            break;
+                        case 4:
+                            echo 'Hit me with your best shot!';
+                            break;
+                        case 5:
+                            echo 'Hit me with your best shot!';
+                            break;
+                        case 6:
+                            echo 'Hit me with your best shot!';
+                            break;
+                        case 7:
+                            echo 'Hit me with your best shot!';
+                            break;
+
+                        default:
+                            echo 'Now is not the time to use that!';
+                            break;
+                    }
+                    ?>
+                </p>
+            </div>
+        </footer>
+
